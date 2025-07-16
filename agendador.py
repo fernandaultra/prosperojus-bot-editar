8b0a398b45581c4bf14ffaaf3ac05b8a4338ae60
Fernanda Selles Ultra Soares <fusoares@timbrasil.com.br>
2025-07-16 17:54:12 -0300

    feat: script de agendamento para execução periódica do envio de dados

---
+++ agendador.py
@@ -0,0 +1,13 @@
+import schedule
+import time
+import os
+
+def rodar_script():
+    os.system("python enviar_dados.py")
+
+# Roda a cada 1 hora
+schedule.every(1).hours.do(rodar_script)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
